9bd1732f38dd08abf09cb2b017bb3a139a605a0c
BiroKrisz <biro.s.kr@gmail.com>
2022-02-08 23:16:24 +0100

    basic routing

--- webshopadmin/src/app/app-routing.module.ts
+++ webshopadmin/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditProductComponent } from './common/edit-product/edit-product.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: DashboardComponent
+  },
+  {
+    path: "product/:id",
+    component: EditProductComponent,
+  },
+  {
+    path: "**",
+    redirectTo: ""
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
