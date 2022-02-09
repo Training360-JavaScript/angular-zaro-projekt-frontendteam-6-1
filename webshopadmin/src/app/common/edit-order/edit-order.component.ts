3e052aba722112d4d7c1a1148fc3aac58b950925
vpeter1 <varga.peter.mobil@gmail.com>
2022-02-09 15:43:28 +0100

    edit-order beta

--- webshopadmin/src/app/common/edit-order/edit-order.component.ts
+++ webshopadmin/src/app/common/edit-order/edit-order.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, switchMap } from 'rxjs';
+import { OrderService } from 'src/app/service/order.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Order } from 'src/app/model/order';
+import { Product } from 'src/app/model/product';
+import { Customer } from 'src/app/model/customer';
+
+
 
 @Component({
   selector: 'app-edit-order',
   templateUrl: './edit-order.component.html',
-  styleUrls: ['./edit-order.component.scss']
+  styleUrls: ['./edit-order.component.scss'],
 })
 export class EditOrderComponent implements OnInit {
+  order$: Observable<Order> = this.activatedRoute.params.pipe(
+    switchMap((params) => this.orderService.get(params['id']))
+  );
 
-  constructor() { }
+  product$: Observable<Product> = this.orderService.getProductAsync(
+    this.order$
+  );
 
-  ngOnInit(): void {
+  customer$: Observable<Customer> = this.orderService.getCustomerAsync(
+    this.order$
+  );
+
+  
+
+  constructor(
+    private orderService: OrderService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+
+  ) {}
+
+  ngOnInit(): void {}
+
+  onUpdate(order: Order): void {
+    this.orderService.update(order).subscribe(
+      (_order) => this.router.navigate(['/']),
+      (err) => console.error(err)
+    );
   }
-
 }
