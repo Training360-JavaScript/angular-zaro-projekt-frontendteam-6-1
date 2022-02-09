6518c578c0bff2e0d65e2487b08cf015b5e3f125
sassi <attila.sassi@gmail.com>
2022-02-09 10:25:18 +0100

    routing + crud.service

--- webshopadmin/src/app/app-routing.module.ts
+++ webshopadmin/src/app/app-routing.module.ts
@@ -1,12 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { EditProductComponent } from './common/edit-product/edit-product.component';
+
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { EditBillComponent } from './common/edit-bill/edit-bill.component';
+import { EditCategoryComponent } from './common/edit-category/edit-category.component';
+import { EditCustomerComponent } from './common/edit-customer/edit-customer.component';
+import { EditOrderComponent } from './common/edit-order/edit-order.component';
+import { EditProductComponent } from './common/edit-product/edit-product.component';
 
 const routes: Routes = [
   {
     path: "",
     component: DashboardComponent
+  },
+  {
+    path: "bill/:id",
+    component: EditBillComponent,
+  },
+  {
+    path: "category/:id",
+    component: EditCategoryComponent,
+  },
+  {
+    path: "customer/:id",
+    component: EditCustomerComponent,
+  },
+  {
+    path: "order/:id",
+    component: EditOrderComponent,
   },
   {
     path: "product/:id",
