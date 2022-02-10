71d0f346864a1988241ebf078d3ccd4d050de5e0
gabtot <gabor.toth.de@digikabel.hu>
2022-02-11 00:22:07 +0100

    apiurl back

--- webshopadmin/src/environments/environment.ts
+++ webshopadmin/src/environments/environment.ts
@@ -4,8 +4,7 @@
 
 export const environment = {
   production: false,
-  // apiUrl: 'https://nettuts.hu/jms/hatsix/',
-  apiUrl: 'http://127.0.0.1:3000/',
+  apiUrl: 'https://nettuts.hu/jms/hatsix/',
 };
 
 /*
